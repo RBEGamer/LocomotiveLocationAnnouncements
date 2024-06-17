8a9378c7d817273c1152cb24f0f9159a7896b915
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2024-06-17 17:30:01 +0200

    Update main.cpp

--- src/GPSLocationSender/src/main.cpp
+++ src/GPSLocationSender/src/main.cpp
@@ -10,12 +10,15 @@
 #include <SPI.h>
 #include <SD.h>
 #include <Arduino_MKRGPS.h>
-
+const int ENABLE_BUTTON_INPUT = 4;
+const int REANOUNCE_BUTTON_INPUT = 5;
 const double GPS_POINT_NEAR = 10.0;
 const double GPS_POINT_TRIGGER_DISTANCE = 2.0;
 const String waypoint_list_filename = "POINTS.CSV"; //10 char lenght limit!!!! file on sd card needs to be uppercase!
 const byte SLAVE_ADDRESS = 42; // I2C ADDRESS OF THE MUSIC PLAYER
 const long SOUND_RESET_TIMEOUT = 10 * 1000; //sound can be replayed after x seconds
+const long last_anounement_row_id = -1;
+
 
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
@@ -65,6 +68,8 @@ void request_sound_playback(long _audiofile_id, String _text = "" ,long _volume 
 
 void setup()
 {
+  pinMode(ENABLE_BUTTON_INPUT, INPUT_PULLUP);
+  pinMode(REANOUNCE_BUTTON_INPUT, INPUT_PULLUP);
   Serial.begin(9600);
   while (!Serial) {
     ;  // wait for serial port to connect. Needed for native USB port only
@@ -131,6 +136,12 @@ void setup()
 
 void loop()
 {
+
+  if(!digitalRead(REANOUNCE_BUTTON_INPUT) && last_anounement_row_id >= 0 && last_anounement_row_id < cp.getRowsCount()){
+    request_sound_playback(String(audiofile[last_anounement_row_id]).toInt(), String(text[last_anounement_row_id]));
+    Serial.println("REANOUNCE_BUTTON_INPUT");
+  }
+
   const char **audiofile = (const char **)cp["audiofile"];
   const char **text = (const char **)cp["text"];
   const char **lat = (const char **)cp["lat"];
@@ -155,28 +166,33 @@ void loop()
       const double course_to_point = TinyGPSPlus::courseTo(latitude, longitude, point_to_check_lat, point_to_check_lon);
 
 
-        Serial.print(latitude, 7);
-        Serial.print(":");
-        Serial.print(longitude, 7);
-        Serial.print(" => ");
-        Serial.print(point_to_check_lat, 7);
-        Serial.print(":");
-        Serial.print(point_to_check_lon, 7);
-        Serial.print(" d=");
-        Serial.print(distance_to_point, 7);
-        Serial.print(" h=");
-        Serial.println(course_to_point, 7);
+      Serial.print(latitude, 7);
+      Serial.print(":");
+      Serial.print(longitude, 7);
+      Serial.print(" => ");
+      Serial.print(point_to_check_lat, 7);
+      Serial.print(":");
+      Serial.print(point_to_check_lon, 7);
+      Serial.print(" d=");
+      Serial.print(distance_to_point, 7);
+      Serial.print(" h=");
+      Serial.println(course_to_point, 7);
 
+      last_anounement_row_id = row;
 
       // TRIGGER
-      if (distance_to_point < GPS_POINT_TRIGGER_DISTANCE)
-      {
-        request_sound_playback(String(audiofile[row]).toInt(), String(text[row]));
-      }else if(distance_to_point < GPS_POINT_NEAR){
-        display_upcomming_waypoint(String(audiofile[row]).toInt(),String(text[row]), distance_to_point);
-        Serial.print(audiofile[row]);
-        Serial.print(":");
-        Serial.println(distance_to_point, 7);
+      if(digitalRead(ENABLE_BUTTON_INPUT)){
+        if (distance_to_point < GPS_POINT_TRIGGER_DISTANCE)
+        {
+          request_sound_playback(String(audiofile[row]).toInt(), String(text[row]));
+        }else if(distance_to_point < GPS_POINT_NEAR){
+          display_upcomming_waypoint(String(audiofile[row]).toInt(),String(text[row]), distance_to_point);
+          Serial.print(audiofile[row]);
+          Serial.print(":");
+          Serial.println(distance_to_point, 7);
+        }
+      }else{
+        Serial.println("ENABLE_BUTTON_INPUT DISBALED");
       }
     }
   }
